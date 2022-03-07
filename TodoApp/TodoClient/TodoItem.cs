8996a3c3cad088ebeb1cf0bbc57c5e5f3ad78171
Sjaak <sjaakrest@gmail.com>
2022-03-07 15:05:09 +0100

    add basic layout of todo app

---
+++ TodoApp/TodoClient/TodoItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoClient
+{
+    internal class TodoItem : INotifyPropertyChanged
+    {
+        string title;
+        string text;
+        bool isCompleted;
+
+        public string Title { get => title; set => title = value; }
+        public string Text { get => text; set => text = value; }
+        public bool IsCompleted { get => isCompleted; set => isCompleted = value; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public TodoItem(string title, string text, bool isCompleted)
+        {
+            this.Title = title;
+            this.Text = text;
+            this.IsCompleted = isCompleted;
+        }
+    }
+}
---
+++ TodoApp/TodoClient/TodoItems.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoClient
+{
+    internal class TodoItems : INotifyPropertyChanged
+    {
+        ObservableCollection<TodoItem> todos = new ObservableCollection<TodoItem>();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public TodoItems()
+        {
+            LoadTodos();
+        }
+
+        private void LoadTodos()
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                TodoItem item = new TodoItem("aaaa", "bbbbbbb" + i, false);
+                todos.Add(item);
+            }
+        }
+
+        public ObservableCollection<TodoItem> Todos
+        {
+            set { SetProperty<ObservableCollection<TodoItem>>(ref todos, value); }
+            get { return todos; }
+        }
+
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+
+            return false;
+        }
+
+        private System.Collections.IEnumerable items1;
+
+        public System.Collections.IEnumerable items { get => items1; set => SetProperty(ref items1, value); }
+    }
+}
