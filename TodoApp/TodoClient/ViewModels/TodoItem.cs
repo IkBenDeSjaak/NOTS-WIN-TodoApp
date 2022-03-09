b1c64f37595a5bd674dd26cf973b955eccdf8bce
Sjaak <sjaakrest@gmail.com>
2022-03-09 16:46:45 +0100

    add viewmodelbase and make TodoItems fetch items from api

--- TodoApp/TodoClient/ViewModels/TodoItem.cs
+++ TodoApp/TodoClient/ViewModels/TodoItem.cs
@@ -4,10 +4,11 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TodoClient.ViewModels;
 
 namespace TodoClient
 {
-    internal class TodoItem : INotifyPropertyChanged
+    public class TodoItem : ViewModelBase
     {
         int id;
         string title;
@@ -16,17 +17,7 @@ namespace TodoClient
 
         public int Id { get => id; set => id = value; }
         public string Title { get => title; set => title = value; }
-        public string Text { get => description; set => description = value; }
+        public string Description { get => description; set => description = value; }
         public bool IsCompleted { get => isComplete; set => isComplete = value; }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        public TodoItem(int id, string title, string description, bool isComplete)
-        {
-            this.Id = id;
-            this.Title = title;
-            this.Text = description;
-            this.IsCompleted = isComplete;
-        }
     }
 }
---
+++ TodoApp/TodoClient/ViewModels/TodoItems.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TodoClient.ViewModels;
+
+namespace TodoClient
+{
+    public class TodoItems : ViewModelBase
+    {
+        ObservableCollection<TodoItem> todos = new ObservableCollection<TodoItem>();
+
+        public TodoItems()
+        {
+            LoadTodos();
+        }
+
+        private async void LoadTodos()
+        {
+            string url = "https://localhost:7275/api/todoitems";
+
+            HttpClient client = new HttpClient();
+
+            var response = await client.GetFromJsonAsync<List<TodoItem>>(url);
+
+            foreach (var item in response)
+            {
+                todos.Add(item);
+            }
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    TodoItem item = new TodoItem(i, "aaaa", "bbbbbbb" + i, false);
+            //    todos.Add(item);
+            //}
+        }
+
+        public ObservableCollection<TodoItem> Todos
+        {
+            set { SetProperty(ref todos, value); }
+            get { return todos; }
+        }
+
+        //protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        //{
+        //    if (!Equals(field, newValue))
+        //    {
+        //        field = newValue;
+        //        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //        return true;
+        //    }
+
+        //    return false;
+        //}
+    }
+}
