c694a147b1dda210e9dd0bbb231e4449783073e0
Sjaak <sjaakrest@gmail.com>
2022-03-11 13:31:06 +0100

    add seed for database

---
+++ TodoApp/TodoBackend/Seed/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO TodoItems (IsComplete, Secret, Description, Title)
+VALUES (false, null, 'Car is very much broken', 'Fix car'),
+(false, null, 'Windows are very dirty, everywhere dirt', 'Clean windows'),
+(true, null, 'There is no food for my cat anymore in the fridge', 'Get food'),
+(true, null, 'Meeting with Anna at 8pm', 'Meet Anna')
